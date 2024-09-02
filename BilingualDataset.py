ec859613fc23b1de63f3cf0557bdc67197744cb6
Chunde <xmuhcd@msn.com>
2024-09-02 09:29:31 -0700

    Fixed more bugs, and typo of functions

--- BilingualDataset.py
+++ BilingualDataset.py
@@ -2,78 +2,98 @@ import torch as tc
 import torch.nn as nn
 from torch.utils.data import Dataset
 
+
 class BilingualDataset(Dataset):
-    
-    def __init__(self, dataset, sourceTokenizer, targetTokenizer, sourceLang, targetLang, sequenceLen)->None:
+
+    def __init__(
+        self,
+        dataset,
+        sourceTokenizer,
+        targetTokenizer,
+        sourceLang,
+        targetLang,
+        sequenceLen,
+    ) -> None:
         super().__init__()
-        
+
         self.dataset = dataset
         self.sourceTokenizer = sourceTokenizer
         self.targetTokenizer = targetTokenizer
         self.sourceLang = sourceLang
         self.targetLang = targetLang
-        self.sosToken = tc.tensor([sourceTokenizer.token_to_id('[SOS]')], dtype=tc.int64)
-        self.eosToken = tc.tensor([sourceTokenizer.token_to_id('[EOS]')], dtype=tc.int64)
-        self.padToken = tc.tensor([sourceTokenizer.token_to_id('[PAD]')], dtype=tc.int64)
+        self.sosToken = tc.tensor(
+            [sourceTokenizer.token_to_id("[SOS]")], dtype=tc.int64
+        )
+        self.eosToken = tc.tensor(
+            [sourceTokenizer.token_to_id("[EOS]")], dtype=tc.int64
+        )
+        self.padToken = tc.tensor(
+            [sourceTokenizer.token_to_id("[PAD]")], dtype=tc.int64
+        )
         self.sequenceLen = sequenceLen
-        
+
     def __len__(self):
         return len(self.dataset)
-    
+
     def __getitem__(self, index):
         sourceTargetPair = self.dataset[index]
-        sourceText = sourceTargetPair['translation'][self.sourceLang]
-        targetText = sourceTargetPair['translation'][self.targetLang]
-        
+        sourceText = sourceTargetPair["translation"][self.sourceLang]
+        targetText = sourceTargetPair["translation"][self.targetLang]
+
         encoderInputTokens = self.sourceTokenizer.encode(sourceText).ids
         decoderInputTokens = self.targetTokenizer.encode(targetText).ids
-        
+
         encoderPaddingTokensNum = self.sequenceLen - len(encoderInputTokens) - 2
-        decoderPaddingTokensNum = self.sequenceLen - len(decoderInputTokens) - 1 # no need ot EOS
-        
+        decoderPaddingTokensNum = (
+            self.sequenceLen - len(decoderInputTokens) - 1
+        )  # no need ot EOS
+
         if encoderPaddingTokensNum < 0 or decoderPaddingTokensNum < 0:
-            raise ValueError('Sequence length is not big enough')
-        
-        encoderInput =  tc.cat(
+            raise ValueError("Sequence length is not big enough")
+
+        encoderInput = tc.cat(
             [
                 self.sosToken,
                 tc.tensor(encoderInputTokens, dtype=tc.int64),
                 self.eosToken,
-                tc.tensor([self.padToken] * encoderPaddingTokensNum, dtype=tc.int64)
+                tc.tensor([self.padToken] * encoderPaddingTokensNum, dtype=tc.int64),
             ]
         )
-        
-        decoderInput =  tc.cat(
+
+        decoderInput = tc.cat(
             [
                 self.sosToken,
                 tc.tensor(decoderInputTokens, dtype=tc.int64),
-                tc.tensor([self.padToken] * decoderPaddingTokensNum, dtype=tc.int64)
+                tc.tensor([self.padToken] * decoderPaddingTokensNum, dtype=tc.int64),
             ]
         )
-        
+
         label = tc.cat(
             [
                 tc.tensor(decoderInputTokens, dtype=tc.int64),
                 self.eosToken,
-                tc.tensor([self.padToken]*decoderPaddingTokensNum, dtype=tc.int64)
+                tc.tensor([self.padToken] * decoderPaddingTokensNum, dtype=tc.int64),
             ]
         )
-        
+
         assert encoderInput.size(0) == self.sequenceLen
         assert decoderInput.size(0) == self.sequenceLen
         assert label.size(0) == self.sequenceLen
-        
-        
+
         return {
-            'encoderInput': encoderInput, # sequence length
-            'decoderInput': decoderInput, # sequence length
-            'encoderMask': (encoderInput != self.padToken),
-            'decoderMask': (decoderInput != self.padToken) & causalMask(decoderInput.size(0)),
-            'label': label,
-            'sourceText': sourceText,
-            'targetText': targetText
-            
+            "encoderInput": encoderInput,  # sequence length
+            "decoderInput": decoderInput,  # sequence length
+            "encoderMask": (encoderInput != self.padToken)
+            .unsqueeze(0)   # need to play with tensor view and slicing
+            .unsqueeze(0)   # lot of to be learnt, or at least be comfortable
+            .int(),         # with it and immediately know how to fix it
+            "decoderMask": (decoderInput != self.padToken).unsqueeze(0).int()
+            & causalMask(decoderInput.size(0)),
+            "label": label,
+            "sourceText": sourceText,
+            "targetText": targetText,
         }
+
 
 def causalMask(size):
     mask = tc.triu(tc.ones(size, size), diagonal=1).type(tc.int)
--- Modules/MultiHeadAttentionBlock.py
+++ Modules/MultiHeadAttentionBlock.py
@@ -2,44 +2,56 @@ import torch as tc
 import torch.nn as nn
 import math
 
-class MultiHeadAttentionBlock(nn.Module)    :
-    
-    def __init__(self, d_model:int, h:int, dropout:float) -> None:
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
         super().__init__()
         self.d_model = d_model
         self.h = h
-        assert self.d_model % h == 0, "dimension of embedding should be divisible by number of headers h"
+        assert (
+            self.d_model % h == 0
+        ), "dimension of embedding should be divisible by number of headers h"
         self.d_k = self.d_model // h
-        self.w_q = nn.Linear(d_model, d_model) # query
-        self.w_k = nn.Linear(d_model, d_model) # key
-        self.w_v = nn.Linear(d_model, d_model) # value
-        
+        self.w_q = nn.Linear(d_model, d_model)  # query
+        self.w_k = nn.Linear(d_model, d_model)  # key
+        self.w_v = nn.Linear(d_model, d_model)  # value
+
         self.w_o = nn.Linear(d_model, d_model)
-        
+
         self.dropout = dropout
-        
-    def forward(self, q, k, v, mask)   :
+
+    def forward(self, q, k, v, mask):
         q_prime = self.w_q(q)
         k_prime = self.w_k(k)
         v_prime = self.w_v(v)
-        
+
         # next, we need to split these metrics into different header metrics
         # (batch, sequence, dim of embedding) -> (batch, sequence, h, d_k) - > (batch, h, seq, dk)
-        q_prime = q_prime.view(q_prime.shape[0], q_prime.shape[1], self.h, self.d_k).transpose(1,2)
-        k_prime = k_prime.view(k_prime.shape[0], k_prime.shape[1], self.h, self.d_k).transpose(1,2)
-        v_prime = v_prime.view(v_prime.shape[0], v_prime.shape[1], self.h, self.d_k).transpose(1,2)
-        
+        q_prime = q_prime.view(
+            q_prime.shape[0], q_prime.shape[1], self.h, self.d_k
+        ).transpose(1, 2)
+        k_prime = k_prime.view(
+            k_prime.shape[0], k_prime.shape[1], self.h, self.d_k
+        ).transpose(1, 2)
+        v_prime = v_prime.view(
+            v_prime.shape[0], v_prime.shape[1], self.h, self.d_k
+        ).transpose(1, 2)
+
         # batch,h seq, dk --> batch, h, seq, seq
         # this operation should be very cautious--23:22
-        attention_score = (q_prime @ k_prime.transpose(-1,-2)) / math.sqrt(self.d_k)
-        
+        attention_score = (q_prime @ k_prime.transpose(-1, -2)) / math.sqrt(self.d_k)
+
         if mask is not None:
             attention_score.masked_fill_(mask == 0, -1e9)
-        self.attention_score = attention_score.softmax(dim = -1)
+        self.attention_score = attention_score.softmax(dim=-1)
         # (batch, h, seq, seq) @ (batch,h, seq, d_k) -> (batch, h, seq, dk)
-        x = attention_score @v_prime
-        
+        x = attention_score @ v_prime
+
         # next we need to reshape the x into (batch, seq, embedding)
-        x = x.transpose(1,2).continuous().view(x.shape[0], x.shape[1], self.h * self.d_k)
-        
+        # Q: tensor view -1 means?
+        # A: he actual value for this dimension will be inferred so that 
+        #    the number of elements in the view matches the original number of elements.
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
         return self.w_o(x)
